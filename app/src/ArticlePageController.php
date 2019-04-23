3ea431d93fc2e5f7775bb7f6c0debbbd7584538b
viccycg <viccyg@digitalpharos.com>
2019-04-23 15:29:01 -0600

    Created form for article comments to be input - inside articlepage controller so that it can be added to page by calling the method; added to article page template;

--- app/src/ArticlePageController.php
+++ app/src/ArticlePageController.php
@@ -3,8 +3,39 @@
 namespace SilverStripe\Lessons;
 
 use PageController;
+use SilverStripe\Forms\Form;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\EmailField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\FormAction;
+use SilverStripe\Forms\RequiredFields;
 
 class ArticlePageController extends PageController
 {
+    // Since this is a public method on the controller,
+    // we can add it to the template by calling $CommentForm
+    public function CommentForm() {
+        $form = Form::create(
+          // controller
+          $this,
+          // name of method that creates the form
+          __FUNCTION__,
+          //  leaving the labels for the fields blank as they're added with placeholder attributes
+          FieldList::create(
+              TextField::create('Name', '')
+                  ->setAttribute('placeholder','Name*'),
+              EmailField::create('Email', '')
+                  ->setAttribute('placeholder','Email*'),
+              TextareaField::create('Comment', '')
+                  ->setAttribute('placeholder','Comment*')
+          ),
+          FieldList::create(
+              FormAction::create('handleComment', 'Post Comment')
+          ),
+          RequiredFields::create('Name', 'Email', 'Comment')
+        );
 
+        return $form;
+    }
 }
--- themes/my-theme/templates/SilverStripe/Lessons/Layout/ArticlePage.ss
+++ themes/my-theme/templates/SilverStripe/Lessons/Layout/ArticlePage.ss
@@ -102,22 +102,8 @@
 						</div>
 
 						<form class="form-style">
-							<div class="col-sm-6">
-								<input type="text" name="Name" placeholder="Name*" class="form-control" />
-							</div>
-
-							<div class="col-sm-6">
-								<input type="email" name="Email" placeholder="Email*" class="form-control"  />
-							</div>
-
-							<div class="col-sm-12">
-								<textarea name="Comment" placeholder="Comment*" class="form-control"></textarea>
-							</div>
-
-							<div class="center">
-								<button type="submit" class="btn btn-default-color btn-lg">Post Comment</button>
-							</div>
-						</form>
+                            $CommentForm
+                        </form>
 					</div>
 				</div>
 
