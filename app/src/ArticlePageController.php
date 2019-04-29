b440f264fd2097ea7976fd285bf3a5242807f0db
viccycg <viccyg@digitalpharos.com>
2019-04-29 15:06:50 -0600

    Updated article page to call comments form with button action method

--- app/src/ArticlePageController.php
+++ app/src/ArticlePageController.php
@@ -13,36 +13,55 @@ use SilverStripe\Forms\RequiredFields;
 
 class ArticlePageController extends PageController
 {
+    // Method has to be whitelisted in order for it to be used in the URL
+    private static $allowed_actions = [
+        'CommentForm',
+    ];
+
     // Since this is a public method on the controller,
     // we can add it to the template by calling $CommentForm
     public function CommentForm() {
+
         $form = Form::create(
-          // controller
-          $this,
-          // name of method that creates the form
-          __FUNCTION__,
-          //  leaving the labels for the fields blank as they're added with placeholder attributes
-          FieldList::create(
-              TextField::create('Name', ''),
-              EmailField::create('Email', ''),
-              TextareaField::create('Comment', '')
-          ),
-          FieldList::create(
-              FormAction::create('handleComment', 'Post Comment')
-                  // force it to render a <button> instead of an <input>
-                  ->setUseButtonTag(true)
-                  ->addExtraClass('btn btn-default-color btn-lg')
-          ),
-          RequiredFields::create('Name', 'Email', 'Comment')
+            // controller
+            $this,
+            // name of method that creates the form
+            __FUNCTION__,
+            //  leaving the labels for the fields blank as they're added with placeholder attributes
+            FieldList::create(
+                TextField::create('Name',''),
+                EmailField::create('Email',''),
+                TextareaField::create('Comment','')
+            ),
+            FieldList::create(
+                FormAction::create('handleComment','Post Comment')
+                    ->setUseButtonTag(true)
+                    ->addExtraClass('btn btn-default-color btn-lg')
+            ),
+            RequiredFields::create('Name','Email','Comment')
         )
-
         ->addExtraClass('form-style');
-
         // add the extra classes and placeholders dynamically.
-        foreach ($form->Fields() as $field) {
+        foreach($form->Fields() as $field) {
             $field->addExtraClass('form-control')
                 ->setAttribute('placeholder', $field->getName().'*');
         }
+
         return $form;
+    }
+
+    public function handleComment($data, $form)
+    {
+        $comment = ArticleComment::create();
+        $comment->Name = $data['Name'];
+        $comment->Email = $data['Email'];
+        $comment->Comment = $data['Comment'];
+        // Create $has_many relation by binding the comment back to the ArticlePage
+        $comment->ArticlePageID = $this->ID;
+        $comment->write();
+
+        $form->sessionMessage('Thanks for your comment!','good');
+
+        return $this->redirectBack();
     }
 }
--- themes/my-theme/templates/SilverStripe/Lessons/Layout/ArticlePage.ss
+++ themes/my-theme/templates/SilverStripe/Lessons/Layout/ArticlePage.ss
@@ -65,35 +65,18 @@
 
 				<h1 class="section-title">Comments</h1>
 
-				<div class="comments">
-					<ul>
-						<li>
-							<img src="images/comment-man.jpg" alt="" />
-							<div class="comment">
-								<a href="#" class="btn btn-default-color">Reply</a>
-								<h3>John Doe<small>30 July, 2014</small></h3>
-								<p>Etiam eget felis lacus. In hendrerit, urna in fringilla interdum, nunc mauris condimentum purus, vel ullamcorper dui risus sed tellus. Nullam lacinia porttitor velit fermentum accumsan. Etiam dui lorem, lobortis pellentesque malesuada nec, lacinia pulvinar libero.</p>
-							</div>
-							<ul>
-								<li>
-									<img src="images/comment-man.jpg" alt="" />
-									<div class="comment">
-										<a href="#" class="btn btn-default-color">Reply</a>
-										<h3>John Doe<small>30 July, 2014</small></h3>
-										<p>In hendrerit, urna in fringilla interdum, nunc mauris condimentum purus, vel ullamcorper dui risus sed tellus. Nullam lacinia porttitor velit fermentum accumsan. Etiam dui lorem, lobortis pellentesque malesuada nec, lacinia pulvinar libero.</p>
-									</div>
-								</li>
-							</ul>
-						</li>
-						<li>
-							<img src="images/comment-woman.jpg" alt="" />
-							<div class="comment">
-								<a href="#" class="btn btn-default-color">Reply</a>
-								<h3>Mary Doe<small>31 July, 2014</small></h3>
-								<p>Etiam eget felis lacus. In hendrerit, urna in fringilla interdum, nunc mauris condimentum purus, vel ullamcorper dui risus sed tellus. Nullam lacinia porttitor velit fermentum accumsan. Etiam dui lorem, lobortis pellentesque malesuada nec, lacinia pulvinar libero. Quisque pretium nunc sed nisl vehicula gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							</div>
-						</li>
-					</ul>
+                <div class="comments">
+                    <ul>
+                        <% loop $Comments %>
+                            <li>
+                                <img src="images/comment-man.jpg" alt="" />
+                                <div class="comment">
+                                    <h3>$Name<small>$Created.Format('j F, Y')</small></h3>
+                                    <p>$Comment</p>
+                                </div>
+                            </li>
+                        <% end_loop %>
+                    </ul>
 
 					<div class="comments-form">
 						<div class="col-sm-12">
